e7bc4cc2a0453bc62b7f9f20a8f675ab04d016fa
Кирилл Воробьев <kovvok070606@yandex.ru>
2024-10-12 22:52:59 +0300

    Testing commit

---
+++ test_file.pas
@@ -0,0 +1,15 @@
+﻿function Factorial(num: integer) : real;
+begin
+  var fact := 1;
+  for var i := 1 to num do
+  begin
+    fact *= i;
+  end;
+  Result := fact;
+end;
+
+begin
+  var N := ReadInteger;
+  Print($'{N}! = {Factorial(N)}');
+  
+end.
