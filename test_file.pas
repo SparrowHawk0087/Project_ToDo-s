4a81fb34f0b870c94bb07c63c8b68b1af6189d3c
Кирилл Воробьев <kovvok070606@yandex.ru>
2024-10-12 23:05:36 +0300

    Added discriptions for function and logging of program working

--- test_file.pas
+++ test_file.pas
@@ -1,4 +1,6 @@
-﻿function Factorial(num: integer) : real;
+﻿{This function calculate factorial of numbers}
+
+function Factorial(num: integer) : real;
 begin
   var fact := 1;
   for var i := 1 to num do
@@ -13,3 +15,16 @@ begin
   Print($'{N}! = {Factorial(N)}');
   
 end.
+
+{10
+10! = 3628800 
+
+1
+1! = 1 
+
+4
+4! = 24
+
+5
+5! = 120  
+}
